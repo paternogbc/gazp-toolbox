6e9b59f81e31de30ca30dda3d8f464d81808a999
Gustavo Paterno <paternogbc@gmail.com>
2019-07-16 09:33:34 -0300

    apply style to functions.

--- code/taxon_name_check/R/protocol_functions.R
+++ code/taxon_name_check/R/protocol_functions.R
@@ -1,82 +1,88 @@
 # Function to perform basic checks on species list!
-basic_check <- function(x){
+basic_check <- function(x) {
   # number of species
   n <- length(x)
-  
+
   # check if is character
   if (!is.character(x)) {
     x <- as.character(x)
     warning("Species names were converted to character")
   }
   # check if species are not in Genus_epithet format
-  if (sum(grepl(pattern = "_", x = x)) != 0) stop(
-    "Check if species names are provided in `Genus epithet` format, instead of 
-    `Genus_epithet`"
-  ) 
-  
+  if (sum(grepl(pattern = "_", x = x)) != 0) {
+    x <- gsub(pattern = "_", replacement = " ", x = x)
+    message("Species list was converted to `Genus epithet` format instead of ", 
+              "`Genus_epithet`")
+  }
   message(
-    paste(n, 'Species were provided!')
-    )
-  
+    paste(n, "Species were provided!")
+  )
+
   return(x)
 }
 
 # Function to parse names (Genus, epithet) on species list!
-parse_name <- function(x){
+parse_name <- function(x) {
   if (!is.character(x)) {
     stop("x must be a character vector! Run basic_check()")
   }
-  n     <- length(x)
-  genus <- sapply(strsplit(x, split = " "),FUN = `[`, 1)
-  epi   <- sapply(strsplit(x, split = " "),FUN = `[`, 2)
-  res   <- data.frame(original_binomial = x,
-                      original_genus = genus,
-                      original_epi   = epi,
-                      stringsAsFactors = FALSE)
-  
-  ngen  <- length(unique(genus))
-                  
+  n <- length(x)
+  genus <- sapply(strsplit(x, split = " "), FUN = `[`, 1)
+  epi <- sapply(strsplit(x, split = " "), FUN = `[`, 2)
+  res <- data.frame(
+    original_binomial = x,
+    original_genus = genus,
+    original_epi = epi,
+    stringsAsFactors = FALSE
+  )
+
+  ngen <- length(unique(genus))
+
   message(paste(
-    n, "Species and", 
-    ngen, "unique Genus names were provided!"))
+    n, "Species and",
+    ngen, "unique Genus names were provided!"
+  ))
   return(res)
 }
 
 # Function to find morphotypes in the species list
-morpho_check <- function(x, pattern = c("sp.", "spp.", "sp", "spp", "NA", " ")){
-  xe  <- x$original_epi
-  xb  <- x$original_binomial
-  
-  wsp    <- lapply(pattern, function(x){
+morpho_check <- function(x, pattern = c("sp.", "spp.", "sp", "spp", "NA", " ")) {
+  x <- pn
+  xe <- x$original_epi
+  xb <- x$original_binomial
+
+  wsp <- lapply(pattern, function(x) {
     which(grepl(pattern = x, x = xe, fixed = T))
-    })
+  })
   names(wsp) <- pattern
-  
-  
-  morp    <- lapply(wsp, function(x) {xb[x]})
-  n_morp  <- length(unique(do.call(c, wsp)))
-  t_morp  <- sapply(morp, length)
+
+
+  morp <- lapply(wsp, function(x) {
+    xb[x]
+  })
+  n_morp <- length(unique(do.call(c, wsp)))
+  t_morp <- sapply(morp, length)
   morp_unique <- unique(do.call(c, wsp))
   x$morphotype <- "no"
-  x[morp_unique ,]$morphotype <- "maybe"
   
   if (sum(t_morp) > 0) {
     print(t_morp)
+    x[morp_unique, ]$morphotype <- "maybe"
     warning(paste(sum(n_morp), "species names might be morphotypes!"))
     warning("Please check original names provided and `output$morphotypes")
   }
-  
-  
+
+
   out <- list(
     res = x,
     morphotypes = t_morp,
-    species     = x[morp_unique, ]$original_binomial
+    species = x[morp_unique, ]$original_binomial
   )
   return(out)
-  }
+}
 
 # function to update all genus names from The Plant List
-tpl_genus <- function(...){
+tpl_genus <- function(...) {
   cli <- crul::HttpClient$new("http://www.theplantlist.org/1.1/browse/-/-/")
   temp <- cli$get()
   temp$raise_for_status()
@@ -87,31 +93,35 @@ tpl_genus <- function(...){
 
 # function to check if a genus exist and get its species list from The
 # Plant List website.
-tpl_genus_search <- function(genus){
+tpl_genus_search <- function(genus) {
   uur <- paste("http://www.theplantlist.org/tpl1.1/search?q=", genus, sep = "")
   cli <- crul::HttpClient$new(uur)
   temp <- cli$get()
   temp$raise_for_status()
   temp <- xml2::read_html(temp$parse("UTF-8"), encoding = "UTF-8")
   all <- xml2::xml_text(xml2::xml_find_all(temp, "//table[@id='tbl-results']//a"))
-  
+
   if (length(all) == 0) {
     message("Genus not found on The Plant List Website")
-    out <- list(tpl_genus = paste(genus, "not found on The Plant List"),
-                species_list = NULL)
+    out <- list(
+      tpl_genus = paste(genus, "not found on The Plant List"),
+      species_list = NULL
+    )
   }
   else {
-    all     <- all[seq(1, length(all), 2)]
-    genus   <- sapply(strsplit(all, split = " "),FUN = `[`, 1)
-    epithet <- sapply(strsplit(all, split = " "),FUN = `[`, 2)
+    all <- all[seq(1, length(all), 2)]
+    genus <- sapply(strsplit(all, split = " "), FUN = `[`, 1)
+    epithet <- sapply(strsplit(all, split = " "), FUN = `[`, 2)
     genus_epithet <- paste(genus, epithet)
-    out <- list(tpl_genus = unique(genus),
-                species_list = genus_epithet)
+    out <- list(
+      tpl_genus = unique(genus),
+      species_list = genus_epithet
+    )
     return(out)
   }
 }
 
-# Function to prepare higher taxonomy from a species list and find 
+# Function to prepare higher taxonomy from a species list and find
 # Genus that do not belong to the TPL list.
 higher_tax <- function(x) {
   tax <- taxonlookup::lookup_table(
@@ -119,31 +129,35 @@ higher_tax <- function(x) {
     by_species = TRUE,
     missing_action = "NA"
   )
-  tax <- 
+  tax <-
     tax %>%
-    dplyr::rename(original_genus    = genus) %>%
-    dplyr::mutate(original_binomial = x,
-           genus_on_tpl = !is.na(family) | !is.na(original_genus) | !is.na(order),) %>%
-    dplyr::select(original_binomial, original_genus,  dplyr::everything())
+    dplyr::rename(original_genus = genus) %>%
+    dplyr::mutate(
+      original_binomial = x,
+      genus_on_tpl = !is.na(family) | !is.na(original_genus) | !is.na(order),
+    ) %>%
+    dplyr::select(original_binomial, original_genus, dplyr::everything())
   # get unmatched genus
-  genus_fail <- tax %>% 
+  genus_fail <- tax %>%
     dplyr::filter(is.na(family) | is.na(original_genus) | is.na(order)) %>%
     dplyr::pull(original_genus)
-  
+
   res <- list(
-    taxonomy_full  = tax,
+    taxonomy_full = tax,
     taxonomy_clean = tax %>% dplyr::filter(genus_on_tpl == TRUE),
     unmatched_genus = genus_fail
   )
   nn <- length(res$unmatched_genus)
   no <- nrow(res$taxonomy_clean)
   if (nn > 0) {
-    message(paste("There are", nn, "unmatched genus names!", "\n",
-                  "You should probably check these manually!"))
+    message(paste(
+      "There are", nn, "unmatched genus names!", "\n",
+      "You should probably check these manually!"
+    ))
     message(paste(res$unmatched_genus, "\n"))
   } else {
     message(paste(no, "species in the Taxonomic table"))
   }
-  
+
   return(res)
 }
--- code/taxon_name_check/R/utils.R
+++ code/taxon_name_check/R/utils.R
@@ -6,17 +6,3 @@ check.packages <- function(pkg){
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
-
-# Usage example
-packages <- c("ggplot2", "afex", "ez", "Hmisc", "pander", "plyr")
-check.packages(packages)
-
-# source R scripts from github
-source_github <- function(x){
-  script <- RCurl::getURL(x, ssl.verifypeer = FALSE)
-  eval(parse(text = script))
-}
-
-# Usave example
-source_github(x = "https://raw.githubusercontent.com/paternogbc/gazp-toolbox/master/code/template/R/functions.R")
-
