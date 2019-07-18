112dd8c8a516832662eac99c774e6885f2ce601b
Gustavo Paterno <paternogbc@gmail.com>
2019-07-17 22:01:17 -0300

    fixed small bigs. Improve clean_tpl function. Add some warnings, flags and messages.

--- code/taxon_name_check/R/protocol_functions.R
+++ code/taxon_name_check/R/protocol_functions.R
@@ -1,18 +1,30 @@
 # Function to perform basic checks on species list!
-basic_check <- function(x) {
-  # number of species
-  n <- length(x)
-
-  # check if is character
+basic_check <- function(x, exclude_duplicated = FALSE) {
+  # is character?
   if (!is.character(x)) {
     x <- as.character(x)
-    warning("Species names were converted to character")
+    warning("Names were converted to a character vector")
   }
+  # number of species
+  n <- length(x)
+  # duplicated names
+  dup <- duplicated(x)
+  if (!sum(duplicated(x), na.rm = TRUE) == 0) {
+    warning("There are duplicated names on the list!")
+    message(paste("duplicated species", x[dup], "\n"))
+    if (exclude_duplicated == TRUE) {
+      warning("Duplicated names were excluded!")
+      x <- x[!dup]
+    }
+  }
+
   # check if species are not in Genus_epithet format
   if (sum(grepl(pattern = "_", x = x)) != 0) {
     x <- gsub(pattern = "_", replacement = " ", x = x)
-    message("Species list was converted to `Genus epithet` format instead of ", 
-              "`Genus_epithet`")
+    message(
+      "Species list was converted to `Genus epithet` format instead of ",
+      "`Genus_epithet`"
+    )
   }
   message(
     paste(n, "Species were provided!")
@@ -35,9 +47,7 @@ parse_name <- function(x) {
     original_epi = epi,
     stringsAsFactors = FALSE
   )
-
   ngen <- length(unique(genus))
-
   message(paste(
     n, "Species and",
     ngen, "unique Genus names were provided!"
@@ -46,16 +56,29 @@ parse_name <- function(x) {
 }
 
 # Function to find morphotypes in the species list
-morpho_check <- function(x, pattern = c("sp.", "spp.", "sp", "spp", "NA", " ")) {
-  x <- pn
+morpho_check <- function(x, pattern = c("sp.", "spp.", "spp", "NA", " "),
+                         correct = FALSE, label = "") {
+  # check if names were parsed
+  sc <- sum(
+    colnames(x) == c(
+      "original_binomial", "original_genus", "original_epi"
+    )
+  )
+  if (sc < 3) {
+    x <- parse_name(x)
+    warning("Names were parsed with 'parse_name()'!!!")
+  }
+
   xe <- x$original_epi
   xb <- x$original_binomial
 
+  # Check if there is true NA in epithet and convert to character
+  xe[is.na(xe)] <- "NA"
+
   wsp <- lapply(pattern, function(x) {
-    which(grepl(pattern = x, x = xe, fixed = T))
+    which(grepl(pattern = x, x = xe, fixed = TRUE))
   })
   names(wsp) <- pattern
-
 
   morp <- lapply(wsp, function(x) {
     xb[x]
@@ -64,19 +87,30 @@ morpho_check <- function(x, pattern = c("sp.", "spp.", "sp", "spp", "NA", " ")) 
   t_morp <- sapply(morp, length)
   morp_unique <- unique(do.call(c, wsp))
   x$morphotype <- "no"
-  
-  if (sum(t_morp) > 0) {
-    print(t_morp)
-    x[morp_unique, ]$morphotype <- "maybe"
+
+  if (sum(t_morp, na.rm = TRUE) > 0) {
+    x[morp_unique, ]$morphotype <- "yes"
     warning(paste(sum(n_morp), "species names might be morphotypes!"))
     warning("Please check original names provided and `output$morphotypes")
   }
 
+  if (n_morp == 0) {
+    message("No morphotypes were found!")
+    correct == FALSE
+  }
+
+  if (correct == TRUE) {
+    x$corrected_epi <- x$original_epi
+    x$corrected_binomial <- x$original_binomial
+    x[x$morphotype == "maybe", ]$corrected_epi <- label
+    x[x$morphotype == "maybe", ]$corrected_binomial <-
+      paste(x[x$morphotype == "maybe", ]$original_genus, label)
+  }
 
   out <- list(
     res = x,
-    morphotypes = t_morp,
-    species = x[morp_unique, ]$original_binomial
+    morphotype = morp,
+    species_list = x[morp_unique, ]$original_binomial
   )
   return(out)
 }
@@ -129,18 +163,22 @@ higher_tax <- function(x) {
     by_species = TRUE,
     missing_action = "NA"
   )
+
   tax <-
     tax %>%
-    dplyr::rename(original_genus = genus) %>%
+    dplyr::rename(submitted_genus = genus) %>%
     dplyr::mutate(
-      original_binomial = x,
-      genus_on_tpl = !is.na(family) | !is.na(original_genus) | !is.na(order),
+      submitted_binomial = x,
+      genus_on_tpl = is.na(family) | is.na(submitted_genus) | is.na(order),
+      genus_on_tpl = !genus_on_tpl
     ) %>%
-    dplyr::select(original_binomial, original_genus, dplyr::everything())
+    dplyr::select(submitted_binomial, submitted_genus, dplyr::everything())
+
   # get unmatched genus
   genus_fail <- tax %>%
-    dplyr::filter(is.na(family) | is.na(original_genus) | is.na(order)) %>%
-    dplyr::pull(original_genus)
+    dplyr::filter(is.na(family) | is.na(submitted_genus) | is.na(order)) %>%
+    dplyr::pull(submitted_genus) %>%
+    unique()
 
   res <- list(
     taxonomy_full = tax,
@@ -148,16 +186,122 @@ higher_tax <- function(x) {
     unmatched_genus = genus_fail
   )
   nn <- length(res$unmatched_genus)
-  no <- nrow(res$taxonomy_clean)
   if (nn > 0) {
     message(paste(
       "There are", nn, "unmatched genus names!", "\n",
       "You should probably check these manually!"
     ))
     message(paste(res$unmatched_genus, "\n"))
-  } else {
-    message(paste(no, "species in the Taxonomic table"))
+  }
+  return(res)
+}
+
+clean_tpl <- function(x, verbose = TRUE) {
+  ## Check species status
+  x$tpl_binomial <- paste(x$New.Genus, x$New.Species)
+  fail <- !x$Plant.Name.Index
+  n_fail <- sum(fail)
+  w_fail <- as.character(x[fail, ]$Taxon)
+  miss <- x$Taxonomic.status == "Misapplied"
+  n_miss <- sum(miss, na.rm = T)
+  w_miss <- as.character(x[miss, ]$Taxon)
+  spel <- x$Typo == TRUE
+  n_spel <- sum(spel, na.rm = T)
+  w_spel <- as.character(x[spel, ]$Taxon)
+
+  xt <-
+    data.frame(
+      submitted_binomial = x$Taxon,
+      submitted_status = x$Taxonomic.status,
+      tpl_genus = x$New.Genus,
+      tpl_epithet = x$New.Species,
+      tpl_binomial = paste(x$New.Genus, x$New.Species),
+      tpl_authority = x$New.Authority,
+      tpl_id = x$New.ID,
+      tpl_status = x$New.Taxonomic.status,
+      tpl_version = x$TPL.version,
+      on_tpl = x$Plant.Name.Index,
+      corrected = spel,
+      misapplied = miss
+    )
+
+  ### detect and clean dupliacted species
+  dup <- duplicated(xt$tpl_binomial)
+  w_dup <- as.character(xt$tpl_binomial[dup])
+  n_dup <- length(w_dup)
+
+  ### add column for duplicated species
+  xt$duplicated <- dup
+
+  ### Create clean table without duplicated species
+  xt_clean <- xt[xt$duplicated == FALSE, ]
+
+  ### Build taxonomic table for species list
+  tax <- higher_tax(x = as.character(xt_clean$tpl_binomial))
+
+  ### Warning and messages
+  if (n_fail != 0) {
+    message(n_fail, " names submitted are not on The Plant List")
+    if (verbose == TRUE) {
+      message("Check these species manually", paste("\n", w_fail))
+    }
   }
 
+  ### Warning and messages
+  if (n_spel != 0) {
+    message(
+      n_spel, " names submitted were corrected automatically!",
+      " Make sure they are right."
+    )
+    if (verbose == TRUE) {
+      message("Check these species manually", paste("\n", w_spel))
+    }
+  }
+
+  ### Warning and messages
+  if (n_dup != 0) {
+    message(
+      n_dup, " names submitted were duplicated after standardization."
+    )
+    if (verbose == TRUE) {
+      message("Check these species manually", paste("\n", w_dup))
+    }
+  }
+
+  ### Warning and messages
+  if (n_miss != 0) {
+    message(
+      n_miss, " names have `Misapplied` as Taxonomic Statatus!",
+      " See `?TPLck()` for details"
+    )
+    if (verbose == TRUE) {
+      message("Check these species manually", paste("\n", w_miss))
+    }
+  }
+
+
+  res <- list(
+    stz_table_all = xt,
+    stz_table_clean = xt_clean,
+    which_fail = w_fail,
+    which_corrected = w_spel,
+    which_duplicated = w_dup,
+    taxonomy = tax$taxonomy_full,
+    unmatched_genus = tax$unmatched_genus
+  )
   return(res)
+}
+
+# Function to fuzzy match TPL genus list
+fuzzy_genus <- function(x, max.distance, genus_list = NULL) {
+  x <- setNames(x, x)
+  if (is.null(genus_list)) {
+    genus_list <- tpl_genus()
+  }
+  wg <- lapply(x, function(x) agrep(
+      pattern = x, x = genus_list,
+      max.distance = max.distance
+    ))
+  out <- lapply(wg, function(x) genus_list[x])
+  return(out)
 }
